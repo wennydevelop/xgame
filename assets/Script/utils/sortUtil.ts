61d05e0d5bd81ad28948998f77b71af69600ba32
zhangwenyuhu <16985775+zhangwenyuhu@users.noreply.github.com>
2020-03-23 17:55:54 +0800

    洗牌

--- assets/Script/utils/sortUtil.ts
+++ assets/Script/utils/sortUtil.ts
@@ -13,27 +13,32 @@ export default class sortUtil{
         a = this.s_bubble(a);
     }
 
-    // 冒泡
+    // 冒泡排序
     s_bubble(arr:number[]):number[]{
         let len:number= arr.length;
         let tmp:number = 0;
+        let isSwap:boolean=true;
         for(let i=0;i<len;i++){
+            // 如果有一次循环中没有产生交换,则说明数组已经是有序的
+            if(!isSwap){ break;}
+            isSwap=false;
             for(let j=i+1;j<len;j++){
                 if(arr[i]>arr[j]){
                     tmp=arr[i];
                     arr[i]=arr[j];
                     arr[j]=tmp;
+                    isSwap=true;
                 }
             }
         }
         return arr;
     }
 
-    // 快速
+    // 快速排序
     s_quick(arr:number[]):number[]{
         let qUpSort = (resArr: number[], left: number, right: number) => {
             if (right <= left) {
-                console.log("sort end left: " + left + " right: " + right);
+                //console.log("sort end left: " + left + " right: " + right);
                 return;
             }
             let initLeft: number = left;
@@ -62,4 +67,37 @@ export default class sortUtil{
         qUpSort(arr, 0, arr.length);
         return arr;
     }
+
+    // 选择排序
+    s_select(arr:number[]):number[]{
+        for(let i=0;i<arr.length;i++){
+            let minIndex:number=0;
+            for(let j=i+1;j<arr.length;j++){
+                if(arr[minIndex]>arr[j]){ 
+                    minIndex=j;
+                }
+            }
+            if(minIndex!=i){
+                let tmp=arr[i];
+                arr[i]=arr[minIndex];
+                arr[minIndex]=tmp;
+            }
+        }
+        return arr;
+    }
+
+    // classic argorithm begin //
+
+    // 扑克洗牌
+    shuffle(arr:number[]):number[]{
+        for(let i=arr.length-1;i>0;i--){
+            let index = Math.floor(Math.random()*i);
+            let tmp = arr[i];
+            arr[i] = arr[index];
+            arr[index]=tmp;
+        }
+        return arr;
+    }
+
+    // classic argorithm end   //
 }
